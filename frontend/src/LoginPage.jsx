95955421cadb3b750a729c74bba8ad45e32f2c9e
llllllll-l <vincent.werelius@gmail.com>
2024-03-18 13:55:12 +0100

    login works

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,45 +1,57 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function LoginPage() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleLogin = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:5191/authentication/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
 
-    console.log('Email', email);
-    console.log('Password', password);
-  }
+      const data = await response.json();
+      console.log(data);
+    } catch (er) {
+      console.error("OBS!!! Something went wrong trying to log in: ", er);
+    }
+  };
 
   return (
     <>
       <div className="card">
-      <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="text"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button onClick={handleLogin}>Login</button>
       </div>
     </>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
