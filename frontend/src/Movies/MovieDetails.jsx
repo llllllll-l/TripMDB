b2076e9b6ee9109b5d274d0f659f4b7e3698e8e4
llllllll-l <vincent.werelius@gmail.com>
2024-03-19 17:15:18 +0100

    ready for merge

--- frontend/src/Movies/MovieDetails.jsx
+++ frontend/src/Movies/MovieDetails.jsx
@@ -22,48 +22,70 @@ function MovieDetails({ movies }) {
   };
 
   useEffect(() => {
+    console.log("useEffect inmoveDetils");
     const movieId = parseInt(id);
     const selectedMovie = movies.find((movie) => movie.id === movieId);
+    console.log("lalalal", selectedMovie);
     if (selectedMovie) {
       setMovie(selectedMovie);
     } else {
       console.error(`Movie with id: ${id} could not be found`);
     }
-  }, [id, movies]);
+  }, [id]);
+
+  console.log("All movies: ", movie);
 
   return (
     <>
       <div className="movie-details">
-        <div className="poster">
-          <img src={movie.image} alt={movie.title} />
-        </div>
-        <div className="info">
-          <h1>{movie.title}</h1>
-          <p>
-            <strong>Release: </strong> {movie.release}
-          </p>
-          {/* //TOTO Add location as th move does not currently have a location */}
-          <p>
-            <strong>Rating: </strong> {movie.rating}
-          </p>
-          <p>
-            <strong>Director: </strong> {movie.director}
-          </p>
+        {movie && movie.locations ? (
+          <>
+            <div className="poster">
+              <img src={movie.image} alt={movie.title} />
+            </div>
+            <div className="info">
+              <h1>{movie.title}</h1>
+              <p>
+                <strong>Release: </strong> {movie.release}
+              </p>
+              {/* //TOTO Add location as th move does not currently have a location */}
+              <p>
+                <strong>Rating: </strong> {movie.rating}
+              </p>
+              <p>
+                <strong>Director: </strong> {movie.director}
+              </p>
 
-          <p>
-            <strong>Year :</strong> {movie.release}
-          </p>
-          <p>
-            <strong>Description: </strong> {movie.description}
-          </p>
-          <button onClick={handleAddTrip}>Add Trip</button>
-          <button onClick={navigateToHome}>Home</button>
-          {showNotification && (
-            <Notification
-              message={`Trip to location(s): ${movie.Country}, with movie-theme: ${movie.Title} was successfully added to your trips!`}
-            />
-          )}
-        </div>
+              <p>
+                <strong>Year :</strong> {movie.release}
+              </p>
+              <p>
+                <strong>Description: </strong> {movie.description}
+              </p>
+              <h2>
+                <strong>Location: </strong>
+              </h2>
+              {movie.locations.map((location) => (
+                <div key={location.id}>
+                  <p>Name: {location.location.locationName}</p>
+                  <p>City: {location.location.city}</p>
+                  <p>Country: {location.location.country}</p>
+                  <p>Latitude: {location.location.latitude}</p>
+                  <p>Longitude: {location.location.longitude}</p>
+                </div>
+              ))}
+              <button onClick={handleAddTrip}>Add Trip</button>
+              <button onClick={navigateToHome}>Home</button>
+              {showNotification && (
+                <Notification
+                  message={`Trip to location(s): ${movie.Country}, with movie-theme: ${movie.Title} was successfully added to your trips!`}
+                />
+              )}
+            </div>
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     </>
   );
