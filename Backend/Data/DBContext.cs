b632e59abb3f320274bcee41abb6cfddc1e7a534
Elias <Elias@soprani.net>
2024-03-19 11:36:55 +0100

    movies done

--- Backend/Data/DBContext.cs
+++ Backend/Data/DBContext.cs
@@ -33,7 +33,18 @@ namespace Backend.Data
             admin.PasswordHash = passwordHasher.HashPassword(admin, "Admin123");
             builder.Entity<ApplicationUser>().HasData(admin);
 
-            }
+            builder.Entity<MovieLocation>()
+               .HasKey(ml => new { ml.MovieId, ml.LocationId });
 
+            builder.Entity<Trip>()
+              .HasOne(t => t.User)
+              .WithMany()
+              .HasForeignKey(t => t.UserId)
+              .IsRequired();
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<Location> Locations { get; set; }
     }
 }
