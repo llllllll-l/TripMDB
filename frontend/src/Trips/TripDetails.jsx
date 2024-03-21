e74595dba89e3cac9c56d0279c77c6c5fd51e400
guro18 <rollgustav95@gmail.com>
2024-03-21 11:12:12 +0100

    cleaned up code a little

--- frontend/src/Trips/TripDetails.jsx
+++ frontend/src/Trips/TripDetails.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { Button } from 'react-bootstrap';
 import { PropTypes } from "prop-types";
 import Card from "react-bootstrap/Card"
